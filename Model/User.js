4527dbcd3c8b3411f4510cfac4b5d14f56368f80
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-20 19:46:44 +0530

    written userModel

--- Model/User.js
+++ Model/User.js
@@ -6,5 +6,39 @@ const userSchema=new mongoose.Schema({
         requires:true,
         minlength:20,
         maxlength:60
-,    }
-})
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match: [/^\S+@\S+\.\S+$/, "Invalid email format"]
+
+    },
+
+    password:{
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:16,
+         match: [
+        /^(?=.*[A-Z])(?=.*[!@#$%^&*])/, 
+        "Password must be 8-16 characters and include at least one uppercase letter and one special character."
+    ]
+    },
+
+    address:{
+        type:String,
+        maxlength:400
+        
+    },
+    role:{
+        type:String,
+        enum:["admin", "user", "owner"],
+        default:"user"
+
+    }
+
+
+},{timestamps:true});
+
+export default mongoose.model("User", userSchema);
