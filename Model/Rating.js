b6964b9a68ae10444cf71e7096e078309657b3d3
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-20 19:56:44 +0530

    added rating schema

--- Model/Rating.js
+++ Model/Rating.js
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 
 const ratingSchema=new mongoose.Schema({
-    store;{
+    store:{
         type: mongoose.Schema.Types.ObjectId,
          ref:"Store",
         required: true
 
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required: true
+    },
+    rating:{
+        type: Number,
+        required: true,
+        min: 1,
+        max:5
     }
+},{timestamps:true})
+
+export default mongoose.model("Rating", ratingSchema);
