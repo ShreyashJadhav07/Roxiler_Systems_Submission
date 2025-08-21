a178f84913fb25d47d1eed09afc8e8b266eed511
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-21 08:43:51 +0530

    added adduser and addstore and getDashboard

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../Model/User";
+import Store from "../Model/Store";
+import Rating from "../Model/Rating";
 
 
 export const adduser=async(req,res)=>{
@@ -39,4 +41,46 @@ export const adduser=async(req,res)=>{
         console.error("Error in adduser:", error);
         res.status(500).json({ message: "Server error", error: error.message });
     }
+}
+
+export const addStore= async(req,res)=>{
+        try{
+            const {email, name, address,ownerId}=req.body;
+            const owner=await User.findById(ownerId);
+            if(!owner || owner.role !== "owner"){
+                return res.status(400).json({ message: "Invalid owner ID or not an owner" });
+            }
+            const store=new Store({
+                name,
+                email,
+                address,
+                owner: owner._id
+            });
+            await store.save();
+            res.status(201).json({ message: "Store created successfully", store });
+
+
+        }
+       catch (error) {
+    console.error("Add store error:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+export const getDashboard= async (req, res) => {
+    try{
+
+        const  totalUsers=await User.countDocuments();
+        const totalStores=await Store.countDocuments();
+        const totalRatings=await Rating.countDocuments();
+        res.json({
+            totalUsers,
+            totalStores,
+            totalRatings
+        });
+
+    }
+     catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 }
